ae314c193634bdb43360c1ee032f9fcb637faab8
roberto08 <rbaltazar5@student.rcc.edu>
2014-09-21 15:56:23 -0700

    Signed-off-by: roberto08 <rbaltazar5@student.rcc.edu>

--- Project/divide.s
+++ Project/divide.s
@@ -2,8 +2,8 @@
 
 	.global _start 
 _start: 
-	mov r1, #49 		/* input a */ 
-	mov r2, #5 		/* input b */ 
+	mov r0, #49 		/* input a */ 
+	mov r1, #5 		/* input b */ 
 
-	add r0, r1, r2 
+	add r0, r0, r1 
 	swi 0 

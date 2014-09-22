e70ac354e2bc9131cefb0a0765cadfab2e957380
roberto08 <rbaltazar5@student.rcc.edu>
2014-09-21 23:52:18 -0700

    Signed-off-by: roberto08 <rbaltazar5@student.rcc.edu>

--- Project/div.s
+++ Project/div.s
@@ -4,11 +4,19 @@ _start:
 	mov r0, #11				/*input a*/
 	mov r1, #5				/*input b*/
 	mov r2, #0 				/*clear the counter register*/
+
+compare	
+	cmp r0, r1				/*compare input a and b*/
+	bneq subtract
+	beq counter
 	
-	cmp r0, r1
+subtract	
 	sub r0, r0, r1
+	add r2, r2, #1	
+	return compare
 	
-	add r2, r2, #11
+counter
+    mov r0, r2	
 	
 	mov r7, #1
 	swi 0

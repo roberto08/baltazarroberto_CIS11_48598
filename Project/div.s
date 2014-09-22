e8f8c2066d9ad6fac40759fbc7109d656120361e
roberto08 <rbaltazar5@student.rcc.edu>
2014-09-22 00:32:53 -0700

    Signed-off-by: roberto08 <rbaltazar5@student.rcc.edu>

--- Project/div.s
+++ Project/div.s
@@ -1,22 +1,23 @@
 /* divide input a and b */
 	.global _start
+	
 _start:
-	mov r0, #100			/*input a*/
+	mov r0, #111			/*input a*/
 	mov r1, #5				/*input b*/
 	mov r2, #0 				/*clear the counter register*/
 
 compare:
 	cmp r0, r1				/*compare input a and b*/
-	bpl subtract
-	bmi counter
+	bpl subtract			/*branch to subtract if positive integer or greater than input b*/
+	bmi counter				/*if less than input b or negative branch to exit*/
 	
 subtract:	
-	sub r0, r0, r1
-	add r2, r2, #1	
-	b compare
+	sub r0, r0, r1			/*subtract input a from b and update register 0*/
+	add r2, r2, #1			/*update the counter or answer of the division*/
+	b compare				/*branch back to compare*/
 	
 counter:
-    mov r0, r2	
+    mov r0, r2				/*moves the counter or answer to register 0*/
 	
 	mov r7, #1
 	swi 0

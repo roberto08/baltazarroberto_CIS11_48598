d171853114b67c84f51589d3afc8069326410a68
roberto08 <rbaltazar5@student.rcc.edu>
2014-09-22 02:35:30 -0700

    Signed-off-by: roberto08 <rbaltazar5@student.rcc.edu>

--- Project/Assignment1_dividing/without_flags.s
+++ Project/Assignment1_dividing/without_flags.s
@@ -4,8 +4,8 @@
 	
 _start:
 	mov r0, #0				/*clear the register*/
-	mov r1, #3				/*input a*/
-	mov r2, #37				/*input b*/
+	mov r1, #24				/*input a*/
+	mov r2, #7				/*input b*/
 
 compare:
 	cmp r1, r2				/*compare input a and b*/

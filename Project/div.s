88f0ec4faa2c0de4cf5eada26b185174a6839d08
roberto08 <rbaltazar5@student.rcc.edu>
2014-09-21 19:48:52 -0700

    Signed-off-by: roberto08 <rbaltazar5@student.rcc.edu>

--- Project/div.s
+++ Project/div.s
@@ -1,7 +1,6 @@
-@ prog3a.s - a simple assembler file
-/* This comment will be ignored by the assembler */
+/* divide input a and b */
 	.global _start
 _start:
-	MOV R0, #49 
-	MOV R7, #1
-	SWI 0
+	mov r0, #49
+	mov r7, #1
+	swi 0

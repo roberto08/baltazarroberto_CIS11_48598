8ac8373b94ef340880c96885411bc757a8537b5e
roberto08 <rbaltazar5@student.rcc.edu>
2014-09-21 20:42:10 -0700

    Signed-off-by: roberto08 <rbaltazar5@student.rcc.edu>

--- Project/div.s
+++ Project/div.s
@@ -3,9 +3,12 @@
 _start:
 	mov r0, #11				/*input a*/
 	mov r1, #5				/*input b*/
+	mov r2, #0 				/*clear the counter register*/
 	
 	cmp r0, r1
 	sub r0, r0, r1
+	
+	add r2, r2, #11
 	
 	mov r7, #1
 	swi 0

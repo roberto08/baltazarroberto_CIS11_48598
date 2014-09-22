ed391a72388fdf162d00121dea60f3ec98cc66e7
roberto08 <rbaltazar5@student.rcc.edu>
2014-09-22 01:16:52 -0700

    Signed-off-by: roberto08 <rbaltazar5@student.rcc.edu>

--- Project/assignment1_dividing_without_flags.s
+++ Project/assignment1_dividing_without_flags.s
@@ -1,23 +1,22 @@
-/* divide input a and b without the flag a/b*/
+/* divide input a and b with the flag a/b*/
 	.global _start
 	
 _start:
-	mov r0, #17				/*input a*/
-	mov r1, #3				/*input b*/
-	mov r2, #0 				/*clear the counter register*/
+	mov r0, #0				/*clear the counter register*/
+	mov r1, #22				/*input a*/
+	mov r2, #7 				/*input b*/
 
 compare:
-	cmp r0, r1				/*compare input a and b*/
+	cmp r1, r2				/*compare input a and b*/
 	bpl subtract			/*branch to subtract, if positive integer or greater than input b*/
-	bmi counter				/*if less than input b or negative, branch to exit*/
+	bmi exit				/*if less than input b or negative, branch to exit*/
 	
 subtract:	
-	sub r0, r0, r1			/*subtract input a from b and update register 0*/
-	add r2, r2, #1			/*update the counter or answer of the division*/
+	sub r1, r1, r2			/*subtract input a from b and update register 0*/
+	add r0, r0, #1			/*update the counter or answer of the division*/
 	b compare				/*branch back to compare*/
 	
-counter:
-    mov r0, r2				/*moves the counter or answer to register 0*/
+exit:
 	
 	mov r7, #1
 	swi 0
--- Project/div.s
+++ Project/div.s
@@ -1,23 +1,19 @@
-/* divide input a and b with the flag a%b*/
+/* divide input a and b without the flag a%b*/
 	.global _start
 	
 _start:
 	mov r0, #9				/*input a*/
 	mov r1, #3				/*input b*/
-	mov r2, #0 				/*clear the counter register*/
 
 compare:
 	cmp r0, r1				/*compare input a and b*/
 	bpl subtract			/*branch to subtract, if positive integer or greater than input b*/
-	bmi counter				/*if less than input b or negative, branch to counter*/
+	bmi exit				/*if less than input b or negative, branch to exit*/
 	
 subtract:	
 	sub r0, r0, r1			/*subtract input a from b and update register 0*/
-@	add r2, r2, #1			/*update the counter or answer of the division*/
 	b compare				/*branch back to compare*/
 	
-counter:
-@    mov r0, r1				/*moves the counter or answer to register 0*/
-	
+exit:
 	mov r7, #1
 	swi 0

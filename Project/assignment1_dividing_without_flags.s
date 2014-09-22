c6572313c747fdd7d0f11feb1c4fc29b6944aea2
roberto08 <rbaltazar5@student.rcc.edu>
2014-09-22 00:47:32 -0700

    Signed-off-by: roberto08 <rbaltazar5@student.rcc.edu>

--- Project/assignment1_dividing_without_flags.s
+++ Project/assignment1_dividing_without_flags.s
@@ -1,4 +1,4 @@
-/* divide input a and b with the flag a%b*/
+/* divide input a and b without the flag a/b*/
 	.global _start
 	
 _start:
@@ -9,15 +9,15 @@ _start:
 compare:
 	cmp r0, r1				/*compare input a and b*/
 	bpl subtract			/*branch to subtract, if positive integer or greater than input b*/
-	bmi counter				/*if less than input b or negative, branch to counter*/
+	bmi counter				/*if less than input b or negative, branch to exit*/
 	
 subtract:	
 	sub r0, r0, r1			/*subtract input a from b and update register 0*/
-@	add r2, r2, #1			/*update the counter or answer of the division*/
+	add r2, r2, #1			/*update the counter or answer of the division*/
 	b compare				/*branch back to compare*/
 	
 counter:
-    mov r0, r1				/*moves the counter or answer to register 0*/
+    mov r0, r2				/*moves the counter or answer to register 0*/
 	
 	mov r7, #1
 	swi 0

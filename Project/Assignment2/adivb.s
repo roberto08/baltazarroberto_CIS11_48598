b9cf3e4a5d38afd5f93410e49efb1636f2145c64
roberto08 <rbaltazar5@student.rcc.edu>
2014-09-28 17:03:25 -0700

    Signed-off-by: roberto08 <rbaltazar5@student.rcc.edu>

---
+++ Project/Assignment2/adivb.s
@@ -0,0 +1,42 @@
+/* divide input a and b with the flag a/b*/
+/*outputs r0 -> a/b*/
+	.global _start
+	
+_start:
+	mov r0, #0				/*answer of a/b*/
+	mov r1, r2 				/*remainder of 1%b*/
+	mov r2, #11				/*input a*/
+	mov r3, #5 				/*input b*/
+	mov r4, #0 
+	mov r5, #0 
+	mov r6, #0 				/*present scale of 10^*/
+	mov r7, #0 				/*subtraction scale factor, r3*r6*/
+	mov r8, #10 			/*shift factor 10*/
+	mov r9, #0 				/*shift test r7*r8*/
+	
+compare:
+	cmp r1, r3				/*compare input a and b*/
+	bge scale				/*branch to scale, if greater than input b*/
+	ble exit				/*if less than input b branch to exit*/
+	
+scale:
+	mov r6, #1 				/*present scale of 10^*/
+	mul r7, r3, r6 			/*subtraction factor*/ 
+	mul r9, r7, r8 			/*test subtraction factor*/
+	cmp r1, r9 				/*test to shift scale by 10*/
+	bgt subtract 			/*branch to subtract if remainder greater than shift test*/
+	
+	mul r6, r8, r6 			/*update scale factor*/
+	mul r7, r3, r6 			/*subtraction factor*/
+	mul r9, r7, r8 			/*test subtraction factor*/
+	bal scale 				/*branch back to scale*/
+
+subtract:
+	add r0, r0, r6			/*update the counter/answer by scale */
+	sub r1, r1, r7			/*subtract input by scale*/
+	cmp r1, r7
+	bge subtract
+	
+exit:
+	mov r7, #1
+	swi 0
---
+++ Project/Assignment2/adivpb.s
@@ -0,0 +1,23 @@
+/* divide input a and b without the flag a%b*/
+/*outputs r1 -> a%b*/
+	.global _start
+	
+_start:
+	mov r0, #0				/*clear the register*/
+	mov r1, #37				/*input a*/
+	mov r2, #6				/*input b*/
+
+compare:
+	cmp r1, r2				/*compare input a and b*/
+	bpl subtract			/*branch to subtract, if positive integer or greater than input b*/
+	bmi exit				/*if less than input b or negative, branch to exit*/
+	
+subtract:	
+	sub r1, r1, r2			/*subtract input a from b and update register 0*/
+	b compare				/*branch back to compare*/
+	
+exit:
+	mov r0, r1 				/* swap register 1 with register 0, r1 -> r0 */
+	
+	mov r7, #1
+	swi 0

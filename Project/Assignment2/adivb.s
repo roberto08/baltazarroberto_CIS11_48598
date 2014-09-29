0ba73314ca9d3bab475174b0b596b5591d2ad3ad
roberto08 <rbaltazar5@student.rcc.edu>
2014-09-28 17:24:07 -0700

    Signed-off-by: roberto08 <rbaltazar5@student.rcc.edu>

--- Project/Assignment2/adivb.s
+++ Project/Assignment2/adivb.s
@@ -3,8 +3,8 @@
 	.global _start
 	
 _start:
-@mov r0, #0				/*answer of a/b*/
-@mov r1, r2 				/*remainder of 1%b*/
+	mov r0, #0				/*answer of a/b*/
+	mov r1, r2 				/*remainder of 1%b*/
 	mov r2, #11				/*input a*/
 	mov r3, #5 				/*input b*/
 	mov r4, #0 
@@ -13,12 +13,9 @@ _start:
 	mov r7, #0 				/*subtraction scale factor, r3*r6*/
 	mov r8, #10 			/*shift factor 10*/
 	mov r9, #0 				/*shift test r7*r8*/
-	mov r1, r2 				/*remainder of 1%b*/
-	mov r0, #0				/*answer of a/b*/
-
 	
 compare:
-	cmp r1, r3				/*compare input a and b*/
+	cmp r2, r3				/*compare input a and b*/
 	bge scale				/*branch to scale, if greater than input b*/
 	ble exit				/*if less than input b branch to exit*/
 	

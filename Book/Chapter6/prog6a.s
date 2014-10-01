3239bf877f49c8e839c861069d76df6380f6fe0b
unknown <rbaltazar5@student.rcc.edu>
2014-10-01 12:59:59 -0700

    Signed-off-by: unknown <rbaltazar5@student.rcc.edu>

--- Book/Chapter6/prog6a.s
+++ Book/Chapter6/prog6a.s
@@ -1,11 +1,12 @@
 /*Program 6a. simple 32-bit addition*/
 /*Perform r0=r1+r2*/
 
-	.global _start:
+	.global _start
+	
 _start:
 	mov r1, #60 		/*get 50 into r1*/
 	mov r2, #60 		/*get 50 into r2*/
-	adds ro, r1, r2 	/*add the two, result in r0*/
+	adds r0, r1, r2 	/*add the two, result in r0*/
 	
 	mov r7, #1 			/*exit through syscall*/
 	swi 0

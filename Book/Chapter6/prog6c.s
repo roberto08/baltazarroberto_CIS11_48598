c763ba1333934b9e3f0e5441d5b0ce446bd396e9
unknown <rbaltazar5@student.rcc.edu>
2014-10-01 13:26:31 -0700

    Signed-off-by: unknown <rbaltazar5@student.rcc.edu>

---
+++ Book/Chapter6/prog6c.s
@@ -0,0 +1,12 @@
+/*multiply two numbers r0=r1*r2*/
+
+	.global _start
+	
+_start:
+	mov r1, #20		/*r1=20*/
+	mov r2, #5		/*r2=5*/
+	mul ro, r1, r2 	/*r0=r1*r2*/
+	
+	mov r7, #1		/*exit through syscall*/
+	swi 0
+	

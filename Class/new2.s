5e7bade954773b42930bc82336f311c0bfab9bda
unknown <rbaltazar5@student.rcc.edu>
2014-10-08 11:58:42 -0700

    Signed-off-by: unknown <rbaltazar5@student.rcc.edu>

--- Class/new2.s
+++ Class/new2.s
@@ -28,3 +28,29 @@ main:
     str lr, [r1]                     /* *r1 ? lr */
  
     ldr r0, address_of_message1      /* r0 ? &message1 */
+    bl printf                        /* call to printf */
+ 
+    ldr r0, address_of_scan_pattern  /* r0 ? &scan_pattern */
+    ldr r1, address_of_number_read   /* r1 ? &number_read */
+    bl scanf                         /* call to scanf */
+ 
+    ldr r0, address_of_message2      /* r0 ? &message2 */
+    ldr r1, address_of_number_read   /* r1 ? &number_read */
+    ldr r1, [r1]                     /* r1 ? *r1 */
+    bl printf                        /* call to printf */
+ 
+    ldr r0, address_of_number_read   /* r0 ? &number_read */
+    ldr r0, [r0]                     /* r0 ? *r0 */
+ 
+    ldr lr, address_of_return        /* lr ? &address_of_return */
+    ldr lr, [lr]                     /* lr ? *lr */
+    bx lr                            /* return from main using lr */
+address_of_message1 : .word message1
+address_of_message2 : .word message2
+address_of_scan_pattern : .word scan_pattern
+address_of_number_read : .word number_read
+address_of_return : .word return
+ 
+/* External */
+.global printf
+.global scanf

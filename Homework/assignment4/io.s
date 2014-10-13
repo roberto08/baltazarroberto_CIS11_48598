eddc6a5f20a48ca11f710c88a28b0569cd8e477a
roberto08 <rbaltazar5@student.rcc.edu>
2014-10-12 23:52:42 -0700

    Signed-off-by: roberto08 <rbaltazar5@student.rcc.edu>

--- Homework/assignment4/io.s
+++ Homework/assignment4/io.s
@@ -17,8 +17,8 @@ return: .word 0
 .global main
 divide:	
   main:
-	mov r2, #25				/*input a*/
-	mov r3, #5	 			/*input b*/
+	mov r2, #526				/*input a*/
+	mov r3, #9	 			/*input b*/
 	mov r4, #0 				/*use to flag a%b*/
 	mov r5, #0 				/*use to swap ro <-> r1*/
 	mov r6, #0 				/*present scale of 10^*/

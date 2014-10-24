aabd413055aa3a393e41d318894630e599ccc79e
roberto08 <rbaltazar5@student.rcc.edu>
2014-10-23 21:27:43 -0700

    Signed-off-by: roberto08 <rbaltazar5@student.rcc.edu>

---
+++ Midterm/prog1.s
@@ -0,0 +1,28 @@
+/*Program develops gross pay for employees*/
+/*Pays straight time for 20 hours worked*/
+/*Pays double time for all hours over 20 but less than 40*/
+/*Pays triple time for hours over 40but less than 60 as maximum*/
+
+.data 
+ 
+message1: .asciz "How many hours did you worked this week? %\n"
+
+message2: .asciz "Your rate pay is $ %\n"
+
+.text
+
+.global main
+main: 
+	push {lr}
+	
+	ldr r0, address_of_message1
+	bl printf
+	
+	ldr r0, address_of_message2
+	bl printf
+	
+	pop {lr}
+	bx lr
+	
+address_of_message1: .word message1
+address_of_message2: .word message2

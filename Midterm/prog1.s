95ed5ee86d6cf78f0802b7a8bb96652a4c857ecb
roberto08 <rbaltazar5@student.rcc.edu>
2014-10-24 23:23:53 -0700

    Signed-off-by: roberto08 <rbaltazar5@student.rcc.edu>

--- Midterm/prog1.s
+++ Midterm/prog1.s
@@ -40,8 +40,8 @@ main:
 	bl printf 						/*Call printf*/
 	
 	ldr r0, address_of_pay_rate		/*Load address_of_pay_rate to r0 as first parameter of scanf*/
-@	ldr r1, r2
-	mov r2, r1 						/*Move pay rate read (second parameter) r1 into top of stack*/
+	ldr r1, r2
+@	mov r2, r1 						/*Move pay rate read (second parameter) r1 into top of stack*/
 @	mov sp, r2						/*Move r2 to top of the stack as second parameter of scanf*/
 	bl scanf 						/*Call to scanf*/
 

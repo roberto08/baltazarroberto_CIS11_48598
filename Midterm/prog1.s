4e59ad52b82401fbc613a7d8318afdcd120d3996
roberto08 <rbaltazar5@student.rcc.edu>
2014-10-25 09:28:49 -0700

    Signed-off-by: roberto08 <rbaltazar5@student.rcc.edu>

--- Midterm/prog1.s
+++ Midterm/prog1.s
@@ -28,25 +28,42 @@ multiplication:
 rate:
 	push {lr}
 
-	cmp r0, #20
+	mov r5, r0 			/*move pay rate to r5*/
+	mov r6, r1 			/*move hours to r6*/
+	mov r7, #0
+	mov r8, #0
+	mov r9, #0
+	
+	cmp r6, #20 		/*if pay rate equal less than 20*/
 	ble straight
 	
-	cmp r0, #40
+	cmp r6, #40 		/*if pay rate equal less than 40*/
 	ble double
 	
-	cmp r0, #60
+	cmp r6, #60
 	ble triple
 
 straight:
-	mov r1, #1
-	ble end
+	mul r7, r1, r6 		@calculate overtime pay
+	bl add_pay
 	
 double:
-    mov r1, #2
-	ble end
+	sub r6, r6, #20 	@double hours to be paid
+    mul r1, r5, #2 		@double pay rate
+	mul r8, r1, r6 		@calculate overtime pay
+	mov r6, #20 		@restore first 20 hours
+	b straight
 	
 triple:
-	mov r1, #3
+	sub r6, r6, #40 	@triple hours to be paid
+    mul r1, r5, #3 		@triple pay rate
+	mul r9, r1, r6 		@calculate overtime pay
+	mov r6, #40 		@restore first 40 hours
+    b double
+	
+add_pay:
+	add r0, r7, r8
+	add r0, r0, r9
 	
 end:	
 	pop {lr}
@@ -64,10 +81,10 @@ main:
 	mov r1, sp						/*Move top of the stack as second parameter of scanf (hours)*/
 	bl scanf 						/*Call scanf*/
 		
-	ldr r0, [sp]  					/*load r0 with hours inputed*/
-	bl rate 						/*Call rate*/
-	sub sp, sp, #4 					/*Make room in stack for type of rate*/
-	str r1, [sp] 					/*Push type of rate to stack*/
+@	ldr r0, [sp]  					/*load r0 with hours inputed*/
+@	bl rate 						/*Call rate*/
+@	sub sp, sp, #4 					/*Make room in stack for type of rate*/
+@	str r1, [sp] 					/*Push type of rate to stack*/
 	
 	ldr r0, address_of_message2 	/*Load message2 to r0 as parameter of printf*/
 	bl printf 						/*Call printf*/
@@ -80,16 +97,18 @@ main:
 
 	ldr r0, [sp]					/*Load into r0 the Pay rate read by scanf*/
 	add sp, sp, #+4 				/*Discard the pay rate read by scanf*/
-	ldr r1, [sp]  					/*Load r1 with type of rate*/
-	add sp, sp, #+4 				/*Discard type of rate*/
-	bl multiplication 				/*Call multiplication*/
+@	ldr r1, [sp]  					/*Load r1 with type of rate*/
+@	add sp, sp, #+4 				/*Discard type of rate*/
+@	bl multiplication 				/*Call multiplication*/
 	
-	mov r1, r0 						/*Move final pay rate to r1*/
+@	mov r1, r0 						/*Move final pay rate to r1*/
 	
-	ldr r0, [sp] 					/*Load into r0 the hours read by scanf*/
+	ldr r1, [sp] 					/*Load into r1 the hours read by scanf*/
 	add sp, sp, #+4 				/*Discard hours read by scanf*/
 	
-	bl multiplication 				/*Call multiplication*/
+	bl rate
+	
+@	bl multiplication 				/*Call multiplication*/
 	
 	mov r1, r0 						/*Move gross pay into r1 as second parameter of printf*/
 	

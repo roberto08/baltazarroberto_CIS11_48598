d745e207bf367c54a901e828121143e9542d8e31
roberto08 <rbaltazar5@student.rcc.edu>
2014-10-29 16:50:43 -0700

    Signed-off-by: roberto08 <rbaltazar5@student.rcc.edu>

--- Project/proj1.s
+++ Project/proj1.s
@@ -14,7 +14,7 @@ main:
 	mov r0,#0 					/* Set time(0) */
 	bl time 					/* Call time */
 	bl srand 					/* Call srand */
-@	mov r4,#0 					/* Setup loop counter */
+	mov r4,#0 					/* Setup loop counter */
 	
 loop_rand: 						/* Create a 2 digit random number */
 	bl rand 					/* Call rand */
@@ -33,8 +33,8 @@ loop_rand: 						/* Create a 2 digit random number */
 	ldr r0, address_of_message 	/* Set &message2 as the first parameter of printf */
 	bl printf 					/* Call printf */
 
-@	add r4,#1
-@	cmp r4,#20
+	add r4,#1
+	cmp r4,#20
 	blt loop_rand
 
 	pop {r4,lr} 				/* Pop the top of the stack and put it in lr */

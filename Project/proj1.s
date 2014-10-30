ae22beedfcf20fb33962db4f8bd8acd619980412
roberto08 <rbaltazar5@student.rcc.edu>
2014-10-29 22:57:38 -0700

    Signed-off-by: roberto08 <rbaltazar5@student.rcc.edu>

--- Project/proj1.s
+++ Project/proj1.s
@@ -26,7 +26,7 @@ random_number:
 								
 	bl divMod 					/* Call divMod function to get remainder */
 	add r1,#1 					/* Remainder in r1 so add 10 giving between 10 and 99 -> 2 digits */
-mov r1, r8
+mov r8, r1
 	ldr r0, address_of_message1 /* Set &message2 as the first parameter of printf */
 	bl printf 					/* Call printf */
 	

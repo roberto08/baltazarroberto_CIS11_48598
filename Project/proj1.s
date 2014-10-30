1a05df3586fcead468c82cf481336bee08833684
roberto08 <rbaltazar5@student.rcc.edu>
2014-10-29 22:06:56 -0700

    Signed-off-by: roberto08 <rbaltazar5@student.rcc.edu>

--- Project/proj1.s
+++ Project/proj1.s
@@ -14,25 +14,25 @@ main:
 	mov r0,#0 					/* Set time(0) */
 	bl time 					/* Call time */
 	bl srand 					/* Call srand */
-@	mov r4,#0 					/* Setup loop counter */
+	mov r4,#0 					/* Setup loop counter */
 	
 @loop_rand: 						/* Create a 2 digit random number */
 	bl rand 					/* Call rand */
 	
-	mov r1,r0,ASR #4 			/* In case random return is negative */
-	add r1, r1, r0	
-@	mov r2,#11 					/* Move 90 to r2 */
+	mov r1,r0,ASR #1 			/* In case random return is negative */
+	
+	mov r2,#11 					/* Move 90 to r2 */
 								/* We want rand()%90+10 so cal divMod with rand()%90 */
 								
-@	bl divMod 					/* Call divMod function to get remainder */
-	add r1,#1 					/* Remainder in r1 so add 10 giving between 10 and 99 -> 2 digits */
+	bl divMod 					/* Call divMod function to get remainder */
+@	add r1,#1 					/* Remainder in r1 so add 10 giving between 10 and 99 -> 2 digits */
 
 	ldr r0, address_of_message 	/* Set &message2 as the first parameter of printf */
 	bl printf 					/* Call printf */
 
-@	add r4,#1
-@	cmp r4,#20
-@	blt loop_rand
+	add r4,#1
+	cmp r4,#20
+	blt loop_rand
 
 	pop {r4,lr} 				/* Pop the top of the stack and put it in lr */
 	bx lr 						/* Leave main */

dcc1f78e78a11bf148e1413ce877b8fb5916d2e9
roberto08 <rbaltazar5@student.rcc.edu>
2014-10-29 22:35:45 -0700

    Signed-off-by: roberto08 <rbaltazar5@student.rcc.edu>

--- Project/proj1.s
+++ Project/proj1.s
@@ -25,7 +25,7 @@ random_number:
 								/* We want rand()%90+10 so cal divMod with rand()%90 */
 								
 	bl divMod 					/* Call divMod function to get remainder */
-	add r1,#1 					/* Remainder in r1 so add 10 giving between 10 and 99 -> 2 digits */
+	add r1,r1,#1 					/* Remainder in r1 so add 10 giving between 10 and 99 -> 2 digits */
 
 	ldr r0, address_of_message1 /* Set &message2 as the first parameter of printf */
 	bl printf 					/* Call printf */

ef9f55fda0afd6577264081bdeb53e6ae44574f4
roberto08 <rbaltazar5@student.rcc.edu>
2014-10-30 17:46:51 -0700

    Signed-off-by: roberto08 <rbaltazar5@student.rcc.edu>

--- Project/proj1.s
+++ Project/proj1.s
@@ -1,18 +1,16 @@
 /*Project 1*/ 
+/*Mastermind*/
 /* -- randTest.s */
 
 .data
 
 message1: .asciz "The random function returned %d\n" 
 
-message2: .asciz "The Dealer cards: %d \n"
-
 .text
 
 random_number:
 	push {r4,lr} 				/* Push lr onto the top of the stack */
-	
-	cmp r0, #11
+
 	beq loop_rand
 	
 	mov r0,#0 					/* Set time(0) */
@@ -25,18 +23,17 @@ loop_rand: 						/* Create a 2 digit random number */
 	
 	mov r1,r0,ASR #1 			/* In case random return is negative */
 	
-	mov r2,#11 					/* Move 90 to r2 */
+	mov r2,#90 					/* Move 90 to r2 */
 								/* We want rand()%90+10 so cal divMod with rand()%90 */
 								
 	bl divMod 					/* Call divMod function to get remainder */
-	add r1,#1 					/* Remainder in r1 so add 10 giving between 10 and 99 -> 2 digits */
-@	mov r8, r1
-@	ldr r0, address_of_message1 /* Set &message2 as the first parameter of printf */
-@	bl printf 					/* Call printf */
+	add r1,#10 					/* Remainder in r1 so add 10 giving between 10 and 99 -> 2 digits */
+	ldr r0, address_of_message1 /* Set &message2 as the first parameter of printf */
+	bl printf 					/* Call printf */
 	
-@	add r4,#1
-@	cmp r4,#5
-@	blt loop_rand 
+	add r4,#1
+	cmp r4,#20
+	blt loop_rand 
 
 	pop {r4,lr} 				/* Pop the top of the stack and put it in lr */
 	bx lr 						/* Leave main */
@@ -45,24 +42,10 @@ loop_rand: 						/* Create a 2 digit random number */
 main: 
 	push {lr} 					/* Push lr on top of the stack*/
 	
-	bl random_number 
-@	mov r1, r8
-	
-	ldr r0, address_of_message1 
-	bl printf 
-	
-	mov r0, #11
-	bl random_number
-@	mov r1, r8
-	
-	ldr r0, address_of_message2
-	bl printf
-	
 	pop {lr} 					/* Pop to top of the stack and put it in lr*/
 	bx lr
 
 address_of_message1: .word message1
-address_of_message2: .word message2
 
 /*External Functions*/
 .global printf

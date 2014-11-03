aea268d22f75b6e4b3cbe752a3f73ac87b014797
unknown <rbaltazar5@student.rcc.edu>
2014-11-03 13:10:20 -0800

    Signed-off-by: unknown <rbaltazar5@student.rcc.edu>

--- Project/test.s
+++ Project/test.s
@@ -6,7 +6,7 @@ message2: .asciz "Your code is %d %d %d %d\n"
 .text
 
 random_number:
-	push {r1, r2, r4, lr} 			/*Push lr, r4, and ,r2 onto the top of the stack*/
+	push {r1, r2, r4, lr} 		/*Push lr, r4, and ,r2 onto the top of the stack*/
 	
 	cmp r4, #4 					/*Test to see if this is the first time random is being called*/
 	beq loop_rand  				/*If second time or more jump to loop_rand*/
@@ -14,7 +14,6 @@ random_number:
 	mov r0,#0 					/*Set time(0)*/
 	bl time 					/*Call time*/
 	bl srand 					/*Call srand*/
-	@mov r4,#0 					/*Setup loop counter*/
 	
 loop_rand: 						/*Create a 2 digit random number*/
 	bl rand 					/*Call rand*/
@@ -24,14 +23,10 @@ loop_rand: 						/*Create a 2 digit random number*/
 	mov r2,#10 					/*Move 10 to r2*/
 
 	bl divMod 					/*Call divMod function to get remainder*/
-	
-	@add r4, r4, #1 				/*Update the loop counter*/
-	@cmp r4,#20
-	@blt loop_rand 
 
 	mov r0, r1
 	
-	pop {r1, r2, r4, lr} 			/*Pop the top of the stack and put it in lr*/
+	pop {r1, r2, r4, lr} 		/*Pop the top of the stack and put it in lr*/
 	bx lr 						/*Leave main*/
 
 .global main 
@@ -39,8 +34,8 @@ main:
 	push {lr} 					/*Push lr on top of the stack*/
 
 	bl random_number			/*Call the random number generator*/
-	sub sp, sp, #4				/*Make room in the stack for number returned*/
-	str r0, [sp]				/*Store number in the stack*/
+@	sub sp, sp, #4				/*Make room in the stack for number returned*/
+@	str r0, [sp]				/*Store number in the stack*/
 	
 	mov r4, #4 					/*Move 4 to r4 to test for only rand in the function*/
 	bl random_number			/*Call the random number generator*/
@@ -57,10 +52,10 @@ main:
 	sub sp, sp, #4				/*Make room in the stack for the number returned*/
 	str r0, [sp]				/*Store the number in the stack*/
 	
-@	mov r4, #4					/*Move 4 to r4 to test for only rand in the function*/
-@	bl random_number			/*Call the random number generator*/
-@	sub sp, sp, #4				/*Make room in the stack for the number returned*/
-@	str r0, [sp]				/*Store the number in the stack*/
+	mov r4, #4					/*Move 4 to r4 to test for only rand in the function*/
+	bl random_number			/*Call the random number generator*/
+	sub sp, sp, #4				/*Make room in the stack for the number returned*/
+	str r0, [sp]				/*Store the number in the stack*/
 		
 	ldr r8, [sp] 				/*Load random number to r8*/
 	add sp, sp, #+4				/*Discard the number from the stack*/

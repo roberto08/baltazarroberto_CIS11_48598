a321bca0c09458d79e6f98649464504b79b9d450
roberto08 <rbaltazar5@student.rcc.edu>
2014-11-02 18:53:40 -0800

    Signed-off-by: roberto08 <rbaltazar5@student.rcc.edu>

--- Project/proj1.s
+++ Project/proj1.s
@@ -4,7 +4,7 @@
 
 .data
 
-@message1: .asciz "Type in your 4 digit code: %d %d %d %d\n"
+message1: .asciz "Type in your 4 digit code: %d %d %d %d\n"
 message5: .asciz "The code is %d %d %d %d\n" 
 
 .text
@@ -44,6 +44,16 @@ loop_rand: 						/* Create a 2 digit random number */
 main: 
 	push {lr} 					/* Push lr on top of the stack*/
 	
+@	sub sp, sp, #16
+	ldr ro, address_of_message1
+@	mov r1, sp
+@	mov r2, sp
+@	mov r3, sp
+@	mov r4, sp  
+	bl scanf
+	
+	add sp, sp, #+4
+	
 	mov r8, #0
 get_code:
 	bl random_number
@@ -55,23 +65,43 @@ get_code:
 	cmp r8, #4
 	ble get_code
 
-code:
-	ldr r1, [sp]
+	ldr r5, [sp]
 	add sp, sp, #+4
 	
-	ldr r2, [sp]
+	ldr r6, [sp]
 	add sp, sp, #+4
 	
-	ldr r3, [sp]
+	ldr r7, [sp]
 	add sp, sp, #+4
 	
-	ldr r4, [sp] 
+	ldr r8, [sp] 
 	add sp, sp, #+4
+	
+	add sp, sp, #+4
+	
+compare_digits	
+	cmp r1, r5
+	addeq r11, r11, #1
+	mov r1, 0x58
+	
+	cmp r2, r6
+	addeq r11, r11, #1
+	mov r2, 0x58
+	
+	cmp r3, r7
+	addeq r11, r11, #1
+	mov r3, 0x58
+	
+	cmp r8, r9
+	addeq r11, r11, #1
+	mov r4, 0x58
 	
 	ldr r0, address_of_message5
 	bl printf
 	
-	add sp, sp, #+4
+@	add sp, sp, #+4
+	
+	
 	
 @	ldr r0, address_of_message1
 @	bl printf
@@ -98,6 +128,7 @@ code:
 	pop {lr} 					/* Pop to top of the stack and put it in lr*/
 	bx lr
 
+address_of_message1: .word message1
 address_of_message5: .word message5
 
 /*External Functions*/

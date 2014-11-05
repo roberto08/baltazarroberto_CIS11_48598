7aa6fbd34504d65aa1bea4f97d38c95a5289aed1
roberto08 <rbaltazar5@student.rcc.edu>
2014-11-04 21:54:53 -0800

    Signed-off-by: roberto08 <rbaltazar5@student.rcc.edu>

--- Project/test2i.s
+++ Project/test2i.s
@@ -7,7 +7,7 @@
 message1: .asciz "Mastermind: Type in your 4 digit code:"
 input: .asciz "%d"
 message2: .asciz "Your code is %d %d %d %d " 
-@message3: .asciz "%d %d\n"
+message3: .asciz "%d %d\n"
 
 .text
 
@@ -39,10 +39,10 @@ user_input:
 	mov r1, sp 					/*Set the stack as the second parameter of scanf*/
 	bl scanf 					/*Call scanf*/
 
-	ldr r4, [sp] 				/*Load user input4 to r4*/
+	ldr r8, [sp] 				/*Load user input4 to r4*/
 	add sp, sp, #+4 			/*Discard the input from the stack*/
 	
-	ldr r3, [sp]				/*Load user input3 to r3*/
+	ldr r7, [sp]				/*Load user input3 to r3*/
 	add sp, sp, #+4				/*Discard the input from the stack*/
 	
 	ldr r2, [sp]				/*Load user input2 to r2*/
@@ -54,10 +54,10 @@ user_input:
 	ldr r0, address_of_message2 /*Print out the correct answer*/
 	bl printf
 	
-	@mov r2, r8
-	@mov r1, r7 
-	@ldr r0, address_of_message3 /*Print out the correct answer*/
-	@bl printf
+	mov r2, r8
+	mov r1, r7 
+	ldr r0, address_of_message3 /*Print out the correct answer*/
+	bl printf
 	
 	pop {lr} 					/* Pop to top of the stack and put it in lr*/
 	bx lr
@@ -65,4 +65,4 @@ user_input:
 address_of_message1: .word message1
 address_of_input: .word input
 address_of_message2: .word message2
-@address_of_message3: .word message3
+address_of_message3: .word message3

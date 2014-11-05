4e1b8d0199c17c85278ae9f52efd8583bcc104f0
roberto08 <rbaltazar5@student.rcc.edu>
2014-11-04 20:52:15 -0800

    Signed-off-by: roberto08 <rbaltazar5@student.rcc.edu>

--- Project/test2i.s
+++ Project/test2i.s
@@ -9,7 +9,7 @@ input1: .asciz "%d"
 input2: .asciz "%d"
 input3: .asciz "%d"
 input4: .asciz "%d\n"
-message2: .asciz "Your code is %d %d %d %d \n" 
+message2: .asciz "Your code is %d %d \n" 
 
 .text
 

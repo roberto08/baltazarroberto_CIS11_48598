a734e3803f1018351f7ce13668d75a8df96abe93
roberto08 <rbaltazar5@student.rcc.edu>
2014-11-05 13:31:07 -0800

    Signed-off-by: roberto08 <rbaltazar5@student.rcc.edu>

--- Homework/assignment6/test.s
+++ Homework/assignment6/test.s
@@ -10,13 +10,11 @@ time: .asciz "the time is %d\n"
 	mov r0, #0
 	bl time
 	
+	mov r1, r0
 	ldr r0, address_of_time
 	bl printf
 
 	pop {lr}
 	bx lr
 
-address_of_message: .word message 
-address_of_scan_format: .word scan_format
-address_of_message2: .word message2
 address_of_time: .word time

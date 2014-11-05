1ebf4283dcf70237615dc23e577c05490a659dc0
roberto08 <rbaltazar5@student.rcc.edu>
2014-11-04 21:02:57 -0800

    Signed-off-by: roberto08 <rbaltazar5@student.rcc.edu>

--- Project/test2i.s
+++ Project/test2i.s
@@ -9,7 +9,7 @@ input1: .asciz "%d"
 input2: .asciz "%d"
 input3: .asciz "%d"
 input4: .asciz "%d\n"
-message2: .asciz "Your code is %d %d \n" 
+message2: .asciz "Your code is %d %d %d \n" 
 
 .text
 
@@ -25,8 +25,8 @@ main:
 	ldr r0, address_of_message2 /*Print out the correct answer*/
 	bl printf
 	
-	mov r1, r3
-	mov r2, r4
+	@mov r1, r3
+	mov r1, r4
 	
 	ldr r0, address_of_message2 /*Print out the correct answer*/
 	bl printf

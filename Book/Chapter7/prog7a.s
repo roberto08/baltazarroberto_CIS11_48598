9afa7e5d4be73e6a6121381cd7c0f1ec6efb4e52
unknown <rbaltazar5@student.rcc.edu>
2014-11-12 12:51:54 -0800

    Signed-off-by: unknown <rbaltazar5@student.rcc.edu>

---
+++ Book/Chapter7/prog7a.s
@@ -0,0 +1,19 @@
+/*How to syscall 4 to write a string*/
+
+.global start
+_start:
+
+	mov r7, #4			@syscall number
+	mov r0, #1			@stdout is monitor
+	mov r2, #19			@string is 19 chars long
+	ldr r1, =string 	@string located at string
+	swi 0
+	
+_exit:
+
+	mov r7, #1 			@exit syscall
+	swi 0
+	
+.data
+string:
+.ascii "Hello World String\n"

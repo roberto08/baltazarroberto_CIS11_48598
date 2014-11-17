0295edd8d1df6ea64377ae3c024c752829aefa4f
unknown <rbaltazar5@student.rcc.edu>
2014-11-17 14:00:03 -0800

    Signed-off-by: unknown <rbaltazar5@student.rcc.edu>

--- Homework/assignment7/divoutput.s
+++ Homework/assignment7/divoutput.s
@@ -18,7 +18,7 @@ return: .word 0
 main:
 
 divide:
-	mov r2, #685			/*input a*/
+	mov r2, #120			/*input a*/
 	mov r3, #9	 			/*input b*/
 	mov r4, #0 				/*use to flag a%b*/
 	mov r5, #0 				/*use to swap ro <-> r1*/

d1ee4a5c8ebf3b2ca63a9e7624cbd5d3cc0ce374
roberto08 <rbaltazar5@student.rcc.edu>
2014-11-19 18:49:21 -0800

    Signed-off-by: roberto08 <rbaltazar5@student.rcc.edu>

--- Homework/assignment7/prog7ab.s
+++ Homework/assignment7/prog7ab.s
@@ -12,20 +12,20 @@ message2: .asciz "The converion to Celsius is: %d\n"
 .text
 
 tempconversion:
-	push {r0, r2, r3, lr} 				/*Push r2, r3, and lr to the stack*/
+	push {r2, r3, lr} 				/*Push r2, r3, and lr to the stack*/
 	
 	Sub r1, r1, #32 					/*Subtract 32 from temperature input*/
 	
-@	ldr r0, =0x8e38f					/*Move 5/9 to r2*/
-	mov r2, r0, lsr#20 					/*Rotate right 20 bits to turn into integer*/
+	ldr r0, =0x8e38f					/*Move 5/9 to r2*/
+	mov r2, r0, asr#20 					/*Rotate right 20 bits to turn into integer*/
 	
-	mul r1, r0, r2 						/*Multiply (f-32)*5/9*/
+	mul r1, r2, r1 						/*Multiply (f-32)*5/9*/
 
-	mov r2, r0, lsr#20 					/*Rotate right 20 bits to turn into integer*/
+@	mov r2, r0, asr#20 					/*Rotate right 20 bits to turn into integer*/
 	
-	mov r1, r2
+@	mov r1, r2
 	
-	pop {r0, r2, r3, lr} 				/*Pop r2, r3 and lr from the stack*/
+	pop {r2, r3, lr} 				/*Pop r2, r3 and lr from the stack*/
 	bx lr 								/*End function and return to main*/
 
 .global main

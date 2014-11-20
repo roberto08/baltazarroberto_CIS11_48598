a76286dd709ea3ea9e083071750b563be65ca1e7
roberto08 <rbaltazar5@student.rcc.edu>
2014-11-19 18:58:05 -0800

    Signed-off-by: roberto08 <rbaltazar5@student.rcc.edu>

--- Homework/assignment7/prog7ab.s
+++ Homework/assignment7/prog7ab.s
@@ -1,5 +1,4 @@
 /*program 7a*/
-/*  https://github.com/ml1150258/LehrMark_CSC11_48598  */
 
 .data
 
@@ -12,20 +11,17 @@ message2: .asciz "The converion to Celsius is: %d\n"
 .text
 
 tempconversion:
-	push {r2, r3, lr} 				/*Push r2, r3, and lr to the stack*/
+	push {r0, r2, r3, lr} 				/*Push r2, r3, and lr to the stack*/
 	
 	Sub r1, r1, #32 					/*Subtract 32 from temperature input*/
 	
 	ldr r0, =0x8e38f					/*Move 5/9 to r2*/
-@	mov r2, r0, asr#20 					/*Rotate right 20 bits to turn into integer*/
 	
-	mul r0, r1, r0 						/*Multiply (f-32)*5/9*/
+	mul r1, r0, r1 						/*Multiply (f-32)*5/9*/
 
 	mov r2, r0, asr#20 					/*Rotate right 20 bits to turn into integer*/
 	
-	mov r0, r2
-	
-	pop {r2, r3, lr} 				/*Pop r2, r3 and lr from the stack*/
+	pop {r0, r2, lr} 				/*Pop r2, r3 and lr from the stack*/
 	bx lr 								/*End function and return to main*/
 
 .global main
@@ -43,21 +39,9 @@ main:
 
 	ldr r1, [sp] 						/*Load the input read to r1*/
 	add sp, sp, #4 						/*Discard the number read from the stack*/
-	
-@	sub r1, r1, #32 					/*Subtract fahrenheit input - 32*/
-	
-@	mov r2, #5 							/*Move 5 to r2 to use multiplication*/
-@	mul r1, r2, r1 						/*Multiply subtracted input by 5*/
-	
-@	mov r2, #9 							/*Move 9 to r2*/
-	
-@	bl divfun 							/*Call division function*/
 
 	bl tempconversion 					/*Call function to convert to Celsius*/
-	
-@	mov r2, r1 							/*Move division to r2 as third parameter of scanf*/
-	mov r1, r0 							/*Move remainder to r1 as second parameter of scanf*/
-	
+
 	ldr r0, address_of_message2 		/*Load first parameter of printf to r0*/
 	bl printf 							/*Call printf*/
 	
